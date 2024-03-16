531ae3b3e28932d36d01347f377d207518fe8013
gl807051 <gulammehboobq@gmail.com>
2024-03-15 20:57:14 -0700

    done

---
+++ WeatherServer/DTO/CountryPopulation.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherServer.DTO
+{
+    public class CountryPopulation
+    {
+        public required string Name { get; set; }
+        
+        public int CountryId { get; set; }
+
+        public int Population { get; set; }
+    }
+}
