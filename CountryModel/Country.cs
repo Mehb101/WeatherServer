feba46634cd8a15e63b781269fa39b49e638765a
gl807051 <gulammehboobq@gmail.com>
2024-03-29 20:52:24 -0700

    noice

--- CountryModel/Country.cs
+++ CountryModel/Country.cs
@@ -12,7 +12,7 @@ public partial class Country
     [Key]
     public int CountryId { get; set; }
 
-    [StringLength(20)]
+    //[StringLength(50)]
     [Unicode(false)]
     public string Name { get; set; } = null!;
 
