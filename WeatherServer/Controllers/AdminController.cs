fca31c9773156ee4988e4798ffb37a597439eb93
gl807051 <gulammehboobq@gmail.com>
2024-04-19 20:40:26 -0700

    Apr19

--- WeatherServer/Controllers/AdminController.cs
+++ WeatherServer/Controllers/AdminController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using WeatherServer.DTO;
 
 namespace WeatherServer.Controllers
 {
@@ -9,10 +11,27 @@ namespace WeatherServer.Controllers
     [ApiController]
     public class AdminController(UserManager<WorldCitiesUsers> userManager, JwtHandler jwtHandler) : ControllerBase
     {
-        [HttpPost]
-        public void Login()
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginRequest loginRequest)
         {
-            
+            WorldCitiesUsers? user = await userManager.FindByNameAsync(loginRequest.Username);
+            if (user == null)
+            {
+                return Unauthorized("Bad Username :^) ");
+            }
+            bool success = await userManager.CheckPasswordAsync(user, loginRequest.Password);
+            if (success)
+            {
+                return Unauthorized(" Wrong Password");
+            }
+            JwtSecurityToken token = await jwtHandler.GetTokenAsync(user);
+            string jwtstring = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new LoginResult
+            {
+                Success = true,
+                Message = "hehehe",
+                Token = jwtstring
+            });
         }
     }
 }
